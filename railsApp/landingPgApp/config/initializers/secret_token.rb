12184d5599692b32b5f06ad629e7767c6f37f684
Ari Akerstein <aakerst@m-C02S31X1G8WM.FogLifterCafe>
2017-01-18 12:15:01 -0800

    updated messaging

---
+++ railsApp/landingPgApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LandingPgApp::Application.config.secret_key_base = '8a204d602ed503bcb070bb4ba602a0a3d3f89dd3aea3641aeb64f0ba6ee145878c0de8f1d8cc85ca20e62d1a2c32156f21ce48c1500bdeb0b3950aa1235fd5d4'
---
+++ railsApp/landingPgApp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LandingPgApp::Application.config.session_store :cookie_store, key: '_landingPgApp_session'
